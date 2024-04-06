978a3115d3f37e88a8eb2dfa31cab1a7bfb7c396
joelflinn <98079616+joelflinn@users.noreply.github.com>
2024-04-06 00:52:49 -0600

    display home page

--- 7wonders/django_project/settings.py
+++ 7wonders/django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wonders_app',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add support for authenticating users
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+]
